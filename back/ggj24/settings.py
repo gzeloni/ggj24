ee8ff2734ecd462558d9eb9e800cb71ebdf54291
brunolcarli <brunolcarli@gmail.com>
2024-01-27 19:43:40 -0300

    CHANGE include prod db settings

--- back/ggj24/settings.py
+++ back/ggj24/settings.py
@@ -93,12 +93,28 @@ WSGI_APPLICATION = 'ggj24.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ENV_REF = os.environ.get('ENV_REF')
+if ENV_REF == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mysql.connector.django',
+            'NAME': os.environ.get('MYSQL_DATABASE', ''),
+            'USER': os.environ.get('MYSQL_USER', ''),
+            'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),
+            'HOST': os.environ.get('MYSQL_HOST', ''),
+            'PORT': os.environ.get('MYSQL_PORT'),
+            'OPTIONS': {
+            'autocommit': True,
+            }
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
