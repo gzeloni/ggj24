909c6e74e7abfe51bb1d9a1759983a07e5217b4d
brunolcarli <brunolcarli@gmail.com>
2024-01-27 07:47:52 -0300

    ADD hoster to match & redo migrations

--- back/api/models.py
+++ back/api/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class MemeMatch(models.Model):
+    hoster = models.ForeignKey('api.UserModel', null=False, on_delete=models.CASCADE)
     reference = models.CharField(max_length=255, null=False, blank=False)
     datetime_open = models.DateTimeField(null=False)
     datetime_close = models.DateTimeField(null=True)
